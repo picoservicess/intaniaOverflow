879249bf75e461fe182387fe5e59a2d5e48656db
ThanatWongsamut <thanat.wongsamut@gmail.com>
2024-09-14 21:16:19 +0700

    fix(asset-service): :pencil2: Change path to import

--- asset-service/src/api/healthCheck/healthCheckRouter.ts
+++ asset-service/src/api/healthCheck/healthCheckRouter.ts
@@ -2,9 +2,9 @@ import { OpenAPIRegistry } from "@asteasolutions/zod-to-openapi";
 import express, { type Request, type Response, type Router } from "express";
 import { z } from "zod";
 
-import { createApiResponse } from "@/api-docs/openAPIResponseBuilders";
-import { ServiceResponse } from "@/common/models/serviceResponse";
-import { handleServiceResponse } from "@/common/utils/httpHandlers";
+import { createApiResponse } from "../../api-docs/openAPIResponseBuilders";
+import { ServiceResponse } from "../../common/models/serviceResponse";
+import { handleServiceResponse } from "../../common/utils/httpHandlers";
 
 export const healthCheckRegistry = new OpenAPIRegistry();
 export const healthCheckRouter: Router = express.Router();
