1f1665f206e88e0c68cee399ffd8ad334f8dc073
ThanatWongsamut <thanat.wongsamut@gmail.com>
2024-09-15 17:25:20 +0700

    feat(asset-service): :lock: Update env validation

--- asset-service/src/common/utils/envConfig.ts
+++ asset-service/src/common/utils/envConfig.ts
@@ -7,4 +7,8 @@ export const env = cleanEnv(process.env, {
   NODE_ENV: str({ devDefault: testOnly("test"), choices: ["development", "production", "test"] }),
   HOST: host({ devDefault: testOnly("localhost") }),
   PORT: port({ devDefault: testOnly(3000) }),
+  AWS_ACCESS_KEY_ID: str(),
+  AWS_SECRET_ACCESS_KEY: str(),
+  AWS_REGION: str(),
+  AWS_S3_BUCKET_NAME: str(),
 });
