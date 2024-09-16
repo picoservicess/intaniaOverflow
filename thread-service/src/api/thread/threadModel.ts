4ba8a4b38ae3bef62e9a4d70feebe1d863b8b1d7
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-09-16 12:51:39 +0700

    Added protofile, schema.prisma, model.ts files

--- thread-service/src/api/thread/threadModel.ts
+++ thread-service/src/api/thread/threadModel.ts
@@ -6,8 +6,13 @@ extendZodWithOpenApi(z);
 export type Thread = z.infer<typeof ThreadSchema>;
 
 export const ThreadSchema = z.object({
-  id: z.number(),
-  content: z.string(),
+  id: z.string(),        // UUID is a string
+  title: z.string(),     // Title as a string
+  content: z.string(),   // Content as a string
+  createdAt: z.date(),   // Date as a JavaScript Date object
+  createdBy: z.string(), // UUID as a string for createdBy
+  updatedAt: z.date(),   // Date as a JavaScript Date object
+  updatedBy: z.string(), // UUID as a string for updatedBy
 });
 
 export const GetThreadSchema = z.object({
