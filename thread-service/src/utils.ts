44c65ba1f2471f91ed2ffe4054ca126dd83cf614
Surapee <suwan.srp@gmail.com>
2024-09-16 22:46:44 +0700

    feat: getAllThreads, getThreadById and createThread

---
+++ thread-service/src/utils.ts
@@ -0,0 +1,21 @@
+export interface Thread {
+  id: string;
+  title: string;
+  body: string;
+  assetUrls: string[];
+  tags: string[];
+  authorId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  isDeleted: boolean;
+}
+
+export interface ThreadList {
+  threads: Thread[];
+}
+
+export interface ThreadId {
+  id: string;
+}
+
+export interface Empty {}
