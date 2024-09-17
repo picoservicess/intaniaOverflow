fc7f2b76e2c43079e4c79545d1083a601b24762a
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-09-17 16:15:46 +0700

    feat: updateThread, DeleteThread, and chang js to ts

--- thread-service/src/server.ts
+++ thread-service/src/server.ts
@@ -1,76 +1,176 @@
 import { sendUnaryData, ServerUnaryCall } from "@grpc/grpc-js";
 import { PrismaClient, Thread } from "@prisma/client";
-import { Empty, ThreadList, ThreadId } from "./utils";
-
-var grpc = require("@grpc/grpc-js");
-var protoLoader = require("@grpc/proto-loader");
+import { Empty, ThreadList, ThreadId } from "./models";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import { prepareUpdateThread } from "./decorator";
 
 const PROTO_PATH = "../proto/thread.proto";
 
 var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  arrays: true,
+    keepCase: true,
+    longs: String,
+    enums: String,
+    arrays: true,
 });
 
-var threadProto = grpc.loadPackageDefinition(packageDefinition);
+var threadProto = grpc.loadPackageDefinition(packageDefinition) as any;
 
 const prisma = new PrismaClient();
+
+console.log("Database connected");
 
 const server = new grpc.Server();
 
 server.addService(threadProto.ThreadService.service, {
-  getAllThreads: async (
-    _: ServerUnaryCall<Empty, ThreadList>,
-    callback: sendUnaryData<ThreadList>
-  ) => {
-    const threads = await prisma.thread.findMany();
-    callback(null, { threads });
-  },
+    getAllThreads: async (
+        _: ServerUnaryCall<Empty, ThreadList>,
+        callback: sendUnaryData<ThreadList>
+    ) => {
+        try {
+            const threads = await prisma.thread.findMany({
+                where: {
+                    isDeleted: false,
+                },
+            });
+            callback(null, { threads });
+        } catch (error) {
+            console.error(`getAllThreads: ${error}`);
+            callback({
+                code: grpc.status.INTERNAL,
+                details: "Interal Server Error",
+            });
+        }
+    },
 
-  getThreadById: async (
-    call: ServerUnaryCall<ThreadId, Thread>,
-    callback: sendUnaryData<Thread>
-  ) => {
-    const thread = await prisma.thread.findUnique({
-      where: call.request,
-    });
-    if (thread) {
-      callback(null, thread);
-    } else {
-      callback({
-        code: grpc.status.NOT_FOUND,
-        details: "Not found",
-      });
-    }
-  },
+    getThreadById: async (
+        call: ServerUnaryCall<ThreadId, Thread>,
+        callback: sendUnaryData<Thread>
+    ) => {
+        try {
+            const thread = await prisma.thread.findUnique({
+                where: {
+                    id: call.request.id,
+                    isDeleted: false,
+                },
+            });
+            if (thread) {
+                callback(null, thread);
+            } else {
+                callback({
+                    code: grpc.status.NOT_FOUND,
+                    details: "Not found",
+                });
+            }
+        } catch (error) {
+            console.error(`getThreadById: ${error}`);
+            callback({
+                code: grpc.status.INTERNAL,
+                details: "Interal Server Error",
+            });
+        }
+    },
 
-  createThread: async (
-    call: ServerUnaryCall<Thread, Thread>,
-    callback: sendUnaryData<Thread>
-  ) => {
-    const thread = await prisma.thread.create({
-      data: call.request,
-    });
-    callback(null, thread);
-  },
+    createThread: async (
+        call: ServerUnaryCall<Thread, Thread>,
+        callback: sendUnaryData<Thread>
+    ) => {
+        try {
+            const thread = await prisma.thread.create({
+                data: call.request,
+            });
+            callback(null, thread);
+        } catch (error) {
+            console.error(`createThread: ${error}`);
+            callback({
+                code: grpc.status.INTERNAL,
+                details: "Interal Server Error",
+            });
+        }
+    },
 
-  // TODO : updateThread
+    updateThread: async (
+        call: ServerUnaryCall<Thread, Thread>,
+        callback: sendUnaryData<Thread>
+    ) => {
+        try {
+            const thread = await prisma.thread.findUnique({
+                where: {
+                    id: call.request.id,
+                    isDeleted: false,
+                },
+            });
 
-  // TODO : deleteThread
+            if (!thread) {
+                callback({
+                    code: grpc.status.NOT_FOUND,
+                    details: "Not found",
+                });
+            }
+            const updatedThread = await prisma.thread.update({
+                where: {
+                    id: call.request.id,
+                },
+                data: prepareUpdateThread(call.request),
+            });
+            callback(null, updatedThread);
+        } catch (error) {
+            console.error(`updateThread: ${error}`);
+            callback({
+                code: grpc.status.INTERNAL,
+                details: "Interal Server Error",
+            });
+        }
+    },
 
-  // TODO : searchThread
+    // TODO : deleteThread
+    deleteThread: async (
+        call: ServerUnaryCall<ThreadId, Empty>,
+        callback: sendUnaryData<Empty>
+    ) => {
+        try {
+            const thread = await prisma.thread.findUnique({
+                where: call.request,
+            });
+
+            if (!thread) {
+                callback({
+                    code: grpc.status.NOT_FOUND,
+                    details: "Not found",
+                });
+            }
+
+            await prisma.thread.update({
+                where: {
+                    id: call.request.id,
+                },
+                data: {
+                    isDeleted: true,
+                },
+            });
+            callback(null, {});
+        } catch (error) {
+            console.error(`deleteThread: ${error}`);
+            callback({
+                code: grpc.status.INTERNAL,
+                details: "Interal Server Error",
+            });
+        }
+    },
+
+    // TODO : searchThread
 });
 
-server.bindAsync(
-  "127.0.0.1:30043",
-  grpc.ServerCredentials.createInsecure(),
-  (err?: Error) => {
-    if (err) {
-      console.error(`Failed to bind server: ${err.message}`);
-      return;
-    }
-    console.log("Thread Service Server running at http://127.0.0.1:30043");
-  }
-);
+try {
+    server.bindAsync(
+        "127.0.0.1:30043",
+        grpc.ServerCredentials.createInsecure(),
+        () => {
+            console.log(
+                "Thread Service Server running at http://127.0.0.1:30043"
+            );
+        }
+    );
+} catch (error) {
+    console.error(`Failed to bind server: ${error}`);
+}
