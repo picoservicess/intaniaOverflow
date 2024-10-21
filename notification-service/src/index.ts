3ebd54d9497c9c89185ea61f2469b27aa4bd4092
paychorn <rujjirap@gmail.com>
2024-10-22 06:48:13 +0700

    feat:🐇 Rabbitmq

--- notification-service/src/index.ts
+++ notification-service/src/index.ts
@@ -12,7 +12,7 @@ mongoose.connect(process.env.DATABASE_URL as string);
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to Database", process.env.DATABASE_URL));
+db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
 
