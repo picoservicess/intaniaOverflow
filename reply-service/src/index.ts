7908bbee3eaa916d7c73adb72fde42b5be680644
ThanatWongsamut <thanat.wongsamut@gmail.com>
2024-10-24 02:44:25 +0700

    Migrate to TypeScript

--- reply-service/src/index.ts
+++ reply-service/src/index.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import cors from "cors";
-import "./loadEnv.mjs";
 import "express-async-errors";
-import replies from "./routes/replies.mjs";
+import replies from "./routes/replies";
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5003;
 const app = express();
 
 app.use(cors());
@@ -14,9 +13,9 @@ app.use(express.json());
 app.use("/replies", replies);
 
 // Global error handling
-app.use((err, _req, res, next) => {
-  res.status(500).send("Uh oh! An unexpected error occured.")
-})
+app.use((err: Error, _req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(500).send("Uh oh! An unexpected error occurred.");
+});
 
 // start the Express server
 app.listen(PORT, () => {
