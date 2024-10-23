cac01cb2f1e19bf5b3b07739fa4f67e950a02abc
ThanatWongsamut <thanat.wongsamut@gmail.com>
2024-10-24 03:30:14 +0700

    Combine everything together

--- notification-service/src/index.ts
+++ notification-service/src/index.ts
@@ -19,6 +19,6 @@ app.use(express.json());
 app.use("/notifications", notificationRouter);
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5002;
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
