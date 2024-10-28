23d02ed3d82f644dada19ef90432679e7aee28ea
ThanatWongsamut <thanat.wongsamut@gmail.com>
2024-10-28 17:50:16 +0700

    Add logging service

---
+++ logging-service/src/index.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import express, { Application } from "express";
+import { PrismaClient } from "@prisma/client";
+import { main as startMQ } from "./mq";
+
+dotenv.config();
+
+const app: Application = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Logging Service is running");
+});
+
+const port = process.env.PORT || 5007;
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+startMQ(prisma).catch((error) => {
+  console.error("❌ Failed to start RabbitMQ:", error);
+  process.exit(1);
+});
