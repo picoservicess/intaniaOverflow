2be6d13575223a1cef731529dd172821079f626c
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-10-29 11:53:21 +0700

    (feat) Added Nginx load balancer for REST services

--- asset-service/src/api/healthCheck/healthCheckRouter.ts
+++ asset-service/src/api/healthCheck/healthCheckRouter.ts
@@ -17,6 +17,7 @@ healthCheckRegistry.registerPath({
 });
 
 healthCheckRouter.get("/", (_req: Request, res: Response) => {
+  console.log("Health check request received");
   const serviceResponse = ServiceResponse.success("Service is healthy", null);
   return handleServiceResponse(serviceResponse, res);
 });
