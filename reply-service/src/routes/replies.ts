90cfdd823927fa758cfaae4c632cd2dcd0af5d92
JinnySarita <6432181821@student.chula.ac.th>
2024-10-30 20:11:54 +0700

    [ADD] health-check for reply-service

--- reply-service/src/routes/replies.ts
+++ reply-service/src/routes/replies.ts
@@ -9,6 +9,11 @@ const prisma = new PrismaClient();
 
 // Create a new Express router instance
 const router = Router();
+router.get("/health-check", (_req: Request, res: Response) => {
+  console.log("💛 Health check request received");
+  res.status(200).json({ status: "OK", message: "Reply-Service is healthy" });
+});
+
 
 /**
  * Route to create a new reply
