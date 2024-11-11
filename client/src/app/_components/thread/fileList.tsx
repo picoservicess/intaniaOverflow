fce2c2f3e53f3e6c0dd8487dff0e41f1d55a8eeb
ThanatWongsamut <thanat.wongsamut@gmail.com>
2024-11-12 00:30:17 +0700

    Fix filename display

--- client/src/app/_components/thread/fileList.tsx
+++ client/src/app/_components/thread/fileList.tsx
@@ -10,7 +10,7 @@ export default function FileList({ assetUrls }: FileListProps) {
 	return (
 		<div className="flex justify-start gap-3 flex-wrap">
 			{assetUrls.map((url) => {
-				const fileName = url.split("/").pop()!;
+				const fileName = decodeURIComponent(url.split("/").pop()!);
 				const nameWithoutPrefix = fileName.split("_").slice(1).join("_");
 				return <FileLink key={url} asset={{ url, name: nameWithoutPrefix }} />;
 			})}
